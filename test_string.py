b5bbce1e73cc328516a02dd8aff604cd1a4c0fdb
annalimx <101759292+annalimx@users.noreply.github.com>
2022-03-16 18:49:50 -0500

    Make failing test

--- test_string.py
+++ test_string.py
@@ -7,7 +7,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertTrue('Foo'.isupper())
 
     def test_split(self):
         s = 'hello world'
