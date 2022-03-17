31c7b63eccff7a874471bdb0dbc58ed9d5f264e6
annalimx <101759292+annalimx@users.noreply.github.com>
2022-03-16 19:26:38 -0500

    update

--- test_string.py
+++ test_string.py
@@ -7,7 +7,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
-        self.assertTrue('Foo'.isupper())
+        self.assertFalse('Foo'.isupper())
 
     def test_split(self):
         s = 'hello world'
@@ -15,6 +15,7 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
 
 if __name__ == '__main__':
     unittest.main()
